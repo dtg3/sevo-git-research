031cc9cc855dd3b98786f75ec8c137c8902796e4
Drew Guarnera <dtg3@zips.uakron.edu>
2013-11-27 12:39:37 -0500

    Support for Repo Collection
    Collection for individual repo or txt repo list.

--- tools/srcstat/srcstat.py
+++ tools/srcstat/srcstat.py
@@ -3,36 +3,25 @@ import sys
 import os
 import sqlite3
 import imp
-from pygit2 import clone_repository
-from pygit2 import Repository
-from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE, GIT_CHECKOUT_SAFE_CREATE
+from gitreap import *
 
 def main(argv):
+  # CLI MAGIC
+  parser = argparse.ArgumentParser(description='Aquire a GitHub Repository and Analyze its comment history')
 
-    # CLI MAGIC
-    parser = argparse.ArgumentParser(description='Aquire a GitHub Repository and Analyze its comment history')
-    # ADD ARGS HERE
-    parser.add_argument('-r','--repo', help='Description for foo argument')
-    
-    # TAKE CLI ARGS AND PARSE ALL THE THINGS
-    cliOpt = parser.parse_args(argv[1:])
+  # ADD ARGS HERE
+  parser.add_argument('-r','--repo', help='Process individual repo')
+  parser.add_argument('-l','--repoList', help='Process repo text file link')
 
-    '''
-    # COLOR FONTING FOR TERMINAL
-    try:
-        imp.find_module('colorama')
-        colorful = True
-        from colorama import init, Fore, Back, Style
-        init()
-        if cliOpt.repo:
-            print cliOpt.repo
-    except ImportError:
-        colorful = False
-        print 'Nope'
-    '''
+  # TAKE CLI ARGS AND PARSE ALL THE THINGS
+  cliOpt = parser.parse_args(argv[1:])
 
-    
-    
-# RUN MAIN - MAKES IT FEEL MORE LIKE A REGUALR C++ APP
+  if cliOpt.repo:
+    print collectRepo(cliOpt.repo)
+
+  if cliOpt.repoList:
+    collectAllRepos(cliOpt.repoList)
+
+  # RUN MAIN - MAKES IT FEEL MORE LIKE A REGUALR C++ APP
 if __name__ == '__main__':
   main(sys.argv)
