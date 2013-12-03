0da909377523d44ea72e07e7541864930816d573
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-12-02 21:47:46 -0500

    Do repo analysis programatically

--- tools/prototype/main.py
+++ tools/prototype/main.py
@@ -5,62 +5,39 @@
 
 from prototype import prototype
 
-ex = prototype()
-ex.init("https://github.com/octocat/Spoon-Knife.git")
-ex.printStats()
+repos = [
+    "https://github.com/octocat/Spoon-Knife.git",
+    "https://github.com/mono/mono.git",
+    "https://github.com/joyent/node.git",
+    "https://github.com/ruby/ruby.git",
+    "https://github.com/python-git/python.git",
+    "https://github.com/mirrors/gcc.git",
+    "https://github.com/pld-linux/koffice.git",
+    "https://github.com/GNOME/gimp.git",
+    "https://github.com/chromium/chromium.git",
+    "https://github.com/linuxmint/Cinnamon.git",
+    "https://github.com/torvalds/linux.git",
+    "https://github.com/apache/hadoop-common.git",
+    "https://github.com/django/django.git",
+    "https://github.com/visionmedia/express.git",
+    "https://github.com/freebsd/freebsd.git",
+    "https://github.com/mono/mono.git",
+    "https://github.com/joyent/node.git",
+    "https://github.com/ruby/ruby.git",
+    "https://github.com/python-git/python.git",
+    "https://github.com/mirrors/gcc.git",
+    "https://github.com/pld-linux/koffice.git",
+    "https://github.com/GNOME/gimp.git",
+    "https://github.com/chromium/chromium.git",
+    "https://github.com/linuxmint/Cinnamon.git",
+    "https://github.com/torvalds/linux.git",
+    "https://github.com/apache/hadoop-common.git",
+    "https://github.com/django/django.git",
+    "https://github.com/visionmedia/express.git",
+    "https://github.com/freebsd/freebsd.git",
+]
 
-monoRepo = prototype()
-monoRepo.init("https://github.com/mono/mono.git")
-monoRepo.printStats()
-
-nodeRepo = prototype()
-nodeRepo.init("https://github.com/joyent/node.git")
-nodeRepo.printStats()
-
-rubyRepo = prototype()
-rubyRepo.init("https://github.com/ruby/ruby.git")
-rubyRepo.printStats()
-
-pythonRepo = prototype()
-pythonRepo.init("https://github.com/python-git/python.git")
-pythonRepo.printStats()
-
-gccRepo = prototype()
-gccRepo.init("https://github.com/mirrors/gcc.git")
-gccRepo.printStats()
-
-officeRepo = prototype()
-officeRepo.init(" https://github.com/pld-linux/koffice.git")
-officeRepo.printStats()
-
-gimpRepo = prototype()
-gimpRepo.init("https://github.com/GNOME/gimp.git")
-gimpRepo.printStats()
-
-chromiumRepo = prototype()
-chromiumRepo.init("https://github.com/chromium/chromium.git")
-chromiumRepo.printStats()
-
-cinnamonRepo = prototype()
-cinnamonRepo.init("https://github.com/linuxmint/Cinnamon.git")
-cinnamonRepo.printStats()
-
-linuxRepo = prototype()
-linuxRepo.init("https://github.com/torvalds/linux.git")
-linuxRepo.printStats()
-
-hadoopRepo = prototype()
-hadoopRepo.init("https://github.com/apache/hadoop-common.git")
-hadoopRepo.printStats()
-
-djangoRepo = prototype()
-djangoRepo.init("https://github.com/django/django.git")
-djangoRepo.printStats()
-
-expressRepo = prototype()
-expressRepo.init("https://github.com/visionmedia/express.git")
-expressRepo.printStats()
-
-freebsdRepo = prototype()
-freebsdRepo.init("https://github.com/freebsd/freebsd.git")
-freebsdRepo.printStats()
+for repo in repos:
+    monoRepo = prototype()
+    monoRepo.init(repo)
+    monoRepo.printStats()
